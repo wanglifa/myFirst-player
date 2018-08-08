a4be9851e30850a6f0a7e1d1d2a5f0d4ae6517fc
wanglifa <1002325418@qq.com>
2018-08-08 17:31:54 +0800

    底部点击右边按钮图片滚动实现

--- js/index.js
+++ js/index.js
@@ -15,16 +15,55 @@ EventCenter.fire('hello','你好')
 
 var Footer = {
     $footer: null,
+    $li: null,
+    $ul: null,
+    $box: null,
+    $leftBtn: null,
+    $rightBtn: null,
     init: function(){
         this.$footer = $('footer');
-        this.bind()
+        this.$li = this.$footer.find('li');
+        this.$ul = this.$footer.find('ul');
+        this.$box = this.$footer.find('.box');
+        this.isToEnd = false;
+        this.isToStart = true;
+        this.$leftBtn = this.$footer.find('.icon-left');
+        this.$rightBtn = this.$footer.find('.icon-right');
         this.render()
+        this.bind()
+        
     },
     bind: function(){
         var _this = this;
-        // $(window).resize(function(){
+        // $(window).on('resize',function(){
+        //     //为什么这个setStyle只有放在resize事件里才能获取到正确的li的数量
         //     _this.setStyle()
         // })
+        var boxWidth = _this.$box.css('width');
+        var liWidth = _this.$footer.find('li').outerWidth(true);
+        var currentRow = Math.floor(parseFloat(boxWidth)/parseFloat(liWidth));
+        console.log(liWidth,boxWidth,currentRow)
+        var ulLeft = currentRow*liWidth;
+        var n = 1;
+        var flag = true;
+        console.log(_this.$ul.css('left'))
+        _this.$rightBtn.on('click',function(){
+            
+            if(!_this.isToEnd){
+                _this.$ul.animate({
+                    left: '-='+ulLeft
+                },300,function(){
+                    if(parseFloat(boxWidth)-parseFloat(_this.$ul.css('left'))>=parseFloat(_this.$ul.css('width'))){
+                        console.log('over')
+                        _this.isToEnd = true;
+                    }
+                })
+            }
+            
+            
+            
+        })
+        
     },
     render: function(){
         var _this = this;
@@ -41,13 +80,26 @@ var Footer = {
         var html = '';
         console.log(channerls)
         channerls.forEach(function(value){
+            //通过拿到JSON里的数据，来拼html
             html += `<li data-channerl-id=${value.channel_id}>
                         <div class="cover" style="background-image:url(${value.cover_small})"></div>
                         <h3>${value.name}</h3>
                      </li>    
             `
         })
-        _this.$footer.find('ul').html(html)
+        _this.$ul.html(html)
+        _this.setStyle()
+
+    },
+    setStyle: function(){
+        var _this = this;
+        var count = _this.$footer.find('li').length;
+        //获得包含margin的单个li的宽度
+        var width = _this.$footer.find('li').outerWidth(true);
+        console.log(count,width)
+        _this.$ul.css({
+            width: (count*width)+'px'
+        })
 
     }
 }
